a711bee949e3517922a515c03a074ace92aadae0
Linas Petrauskas <linas.petrauskas@gmail.com>
2024-03-12 22:52:58 +0100

    Use relative path in data lookup to make it work on Github pages.

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ var namesArray = null;
 export async function loadNames() {
   if (namesArray === null) {
     try {
-      const response = await fetch('/data/names.txt');
+      const response = await fetch('data/names.txt');
       const data = await response.text();
       namesArray = data.split('\n').filter((name) => name.trim() !== ''); // Split by newline and filter out empty lines
     } catch (error) {
